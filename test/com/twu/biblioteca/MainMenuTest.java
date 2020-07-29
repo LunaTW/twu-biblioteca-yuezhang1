0a0be9430aa8287989c3f3a08366ae76ba4f43ed
LunaTW <yue.zhang1@thoughtworks.com>
2020-07-30 00:02:02 +0800

    1.4 prepare add options in mainmenu
    在原本只显示一条的基础上，增加了可能没有主菜单的选项，因此考类给所有的可能性放在一个option的list中

--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -4,7 +4,8 @@ package com.twu.biblioteca;
         （1.4）点击查看主菜单：是否有操作选项（此时只有选项：书籍清单）
         （1.5）点击无效的选项时，是否报错
         【测试】【给出主菜单选项1.书籍 2，电影等等附加功能】
-        1. 输入1，则进入书籍清单界面
+        1. 没有option
+        2. 一个option
         2. 输入88，则报错
  */
 
@@ -16,6 +17,15 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class MainMenuTest {
+    @Test
+    public void MainMenuWasPrintedWhenZeroOption(){
+        ByteArrayOutputStream MainMenuOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(MainMenuOutput));
+        MainMenu.MainMenu();
+        assertEquals("", MainMenuOutput.toString());
+
+    }
+
     private String OneOption="1- List of books\n";
     @Test
     public void MainMenuWasPrintedWhenOneOption(){
