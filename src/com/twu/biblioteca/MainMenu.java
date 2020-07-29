33800447304b95e158bed9e80e6d94664a2e3551
LunaTW <yue.zhang1@thoughtworks.com>
2020-07-30 01:10:38 +0800

    1. 4 0 option & 1 option
    测试主菜单有0个或1个option

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,9 +1,17 @@
 package com.twu.biblioteca;
 
-public class MainMenu {
+import java.util.List;
+import java.util.stream.IntStream;
 
-    public static void MainMenu(){
-        System.out.println("1- List of books");
+public class MainMenu {
+    private List<String> options;
+
+    public MainMenu(List<String> options){
+        this.options = options;
+    }
+
+    public void PrintAllMenuList(){
+        IntStream.range(0, options.size()).forEach(i -> System.out.println(i + 1 + "- " + options.get(i)));//^^^
     }
 }
 
--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -6,33 +6,45 @@ package com.twu.biblioteca;
         【测试】【给出主菜单选项1.书籍 2，电影等等附加功能】
         1. 没有option
         2. 一个option
-        2. 输入88，则报错
+        3. 输入88，则报错
+        ======> 合并
+        1. 打印出所有option
+        2。选择88，报错
  */
 
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainMenuTest {
     @Test
     public void MainMenuWasPrintedWhenZeroOption(){
+        //List<String> options = new ArrayList<>();
+        //MainMenu mainMenu = new MainMenu(options);
+        MainMenu mainMenu = new MainMenu(new ArrayList<>());
         ByteArrayOutputStream MainMenuOutput = new ByteArrayOutputStream();
         System.setOut(new PrintStream(MainMenuOutput));
-        MainMenu.MainMenu();
+        mainMenu.PrintAllMenuList();
         assertEquals("", MainMenuOutput.toString());
-
     }
 
-    private String OneOption="1- List of books\n";
+    private String option1="List of books";
+
     @Test
     public void MainMenuWasPrintedWhenOneOption(){
+        List<String> options = new ArrayList<>(Arrays.asList(option1));
+        MainMenu mainMenu = new MainMenu(options);
+
         ByteArrayOutputStream MainMenuOutput = new ByteArrayOutputStream();
         System.setOut(new PrintStream(MainMenuOutput));
-        MainMenu.MainMenu();
-        assertEquals(OneOption, MainMenuOutput.toString());
+        mainMenu.PrintAllMenuList();
+        assertEquals("1- " + option1 + "\n", MainMenuOutput.toString());
     }
 }
 
