5787ad0b13e902777bc83ae0f76234d82fb403c0
LunaTW <yue.zhang1@thoughtworks.com>
2020-07-31 15:08:15 +0800

    1.5 invalid option-pass

--- test/com/twu/biblioteca/MainMenuTest.java
+++ test/com/twu/biblioteca/MainMenuTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 /*      （1.4）点击查看主菜单：是否有操作选项（此时只有选项：书籍清单）
         （1.5）点击无效的选项时，是否报错
         【测试】【给出主菜单选项1.书籍 2，电影等等附加功能】
@@ -44,17 +45,61 @@ public class MainMenuTest {
         mainMenu.PrintAllMenuList();
         assertEquals("1- " + option1 + "\n", MainMenuOutput.toString());
     }
-
+    //*********************************** 1.5 当我选择无效选项时得到通知 *********************************** //
     @Test
-    //此时只有一个option
-    public void GetNotifiedWhenChoseInvalidOption(){
+    //此时只有一个option,只有 1 是正确的，其他如 3，list，-8，3.5 均无效
+    public void GetNotifiedWhenChoseInvalidOption_OtherNumber(){
         options = new ArrayList<>(Arrays.asList(option1));
         mainMenu = new MainMenu(options);
-        System.setIn(new ByteArrayInputStream("3".getBytes()));
+        System.setIn(new ByteArrayInputStream("7".getBytes()));
         //mainMenu.PrintAllMenuList();
         mainMenu.UserSelectOptions();
         assertEquals(InvalidOption + "\n", MainMenuOutput.toString());
     }
+
+    @Test
+    public void GetNotifiedWhenChoseInvalidOption_Decimal(){
+        options = new ArrayList<>(Arrays.asList(option1));
+        mainMenu = new MainMenu(options);
+        System.setIn(new ByteArrayInputStream("1.5".getBytes()));
+        //mainMenu.PrintAllMenuList();
+        mainMenu.UserSelectOptions();
+        assertEquals(InvalidOption + "\n", MainMenuOutput.toString());
+    }
+
+    @Test
+    public void GetNotifiedWhenChoseInvalidOption_NegativeNumber(){
+        options = new ArrayList<>(Arrays.asList(option1));
+        mainMenu = new MainMenu(options);
+        System.setIn(new ByteArrayInputStream("-2".getBytes()));
+        //mainMenu.PrintAllMenuList();
+        mainMenu.UserSelectOptions();
+        assertEquals(InvalidOption + "\n", MainMenuOutput.toString());
+    }
+
+    @Test
+    public void GetNotifiedWhenChoseInvalidOption_NonNumber(){
+        options = new ArrayList<>(Arrays.asList(option1));
+        mainMenu = new MainMenu(options);
+        System.setIn(new ByteArrayInputStream("List".getBytes()));
+        //mainMenu.PrintAllMenuList();
+        mainMenu.UserSelectOptions();
+        assertEquals(InvalidOption + "\n", MainMenuOutput.toString());
+    }
+
+    //*********************************** （1.2）查看书籍清单 *********************************** //
+    @Test // 选择了有效的选项，则没有报错
+    public void GetNotifiedWhenChosevalidOption(){
+        options = new ArrayList<>(Arrays.asList(option1));
+        mainMenu = new MainMenu(options);
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
+        mainMenu.UserSelectOptions();
+        assertNotEquals(InvalidOption + "\n", MainMenuOutput.toString());
+    }
+
+    @
+
+
 }
 
 
--- test/com/twu/biblioteca/WelcomeMessageTest.java
+++ test/com/twu/biblioteca/WelcomeMessageTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.*;
 //【测试】1. 程序init之后，应出现欢迎界面 https://stackoverflow.com/questions/33569920/how-to-test-system-out-println-by-mocking
 public class WelcomeMessageTest {
     private String WelcomeMessagecheck = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
+
+    //******************************************* （1.1）用户看到欢迎消息 ******************************//
     @Test
     public void ShouldHaveAWelcomeMessage(){
         ByteArrayOutputStream OutputInformation= new ByteArrayOutputStream();
